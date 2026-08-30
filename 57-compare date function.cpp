cb74f8905bf21b5b43522af86307a7ff2d850add
Abdallah-Mo-Amin <abdallahamin567@gmail.com>
2026-08-30 18:43:25 +0300

    Create 57-compare date function.cpp

---
+++ 57-compare date function.cpp
@@ -0,0 +1,130 @@
+#pragma warning(disable : 4996)
+#include <iostream>
+#include <cstdlib>  
+#include <iomanip> // this library stored the std::setw
+#include <cmath>
+#include <string> // this library stired the string object
+#include <vector>
+#include <cctype>
+#include<sstream>
+
+using namespace std;
+
+struct stDate
+{
+    short Day = 0;
+    short Month = 0;
+    short Year = 0;
+};
+
+bool IsDate1EqualToDate2(stDate Date1, stDate Date2)
+{
+    return (Date1.Year == Date2.Year) ? ((Date1.Month == Date2.Month) ? ((Date1.Day == Date2.Day) ? true : false) : false) : false;
+}
+
+bool IsDate1BeforeDate2(stDate Date1, stDate Date2)
+{
+    return (Date1.Year < Date2.Year) ? true : ((Date1.Year == Date2.Year) ? (Date1.Month < Date2.Month ? true : (Date1.Month == Date2.Month ? Date1.Day < Date2.Day : false)) : false);
+}
+
+bool IsDate1AfterDate2(stDate Date1, stDate Date2)
+{
+    return !(IsDate1BeforeDate2(Date1, Date2)) && !(IsDate1EqualToDate2(Date1, Date2));
+}
+
+
+enum enDateCompare { Before = -1, Equal = 0, After = 1 };
+
+enDateCompare CompareDates(stDate Date1, stDate Date2)
+{
+   
+    if (IsDate1BeforeDate2(Date1, Date2))
+        return enDateCompare::Before;
+    if (IsDate1EqualToDate2(Date1, Date2))
+        return enDateCompare::Equal;
+
+    return enDateCompare::After;
+    
+}
+
+short ReadYear()
+{
+    short Number = 0;
+
+    cout << "Please enter a year? ";
+    cin >> Number;
+
+    while (cin.fail() || cin.peek() != '\n')
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        cout << "Invalid Number , Enter a valid one : ";
+        cin >> Number;
+
+    }
+
+    return Number;
+
+}
+
+short ReadMonth()
+{
+    short Month = 0;
+
+    cout << "Please enter a Month? ";
+    cin >> Month;
+
+    while (cin.fail() || cin.peek() != '\n')
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        cout << "Please enter a Month? ";
+        cin >> Month;
+    }
+
+    return Month;
+}
+
+short ReadDay()
+{
+    short Day = 0;
+    cout << "\nPlease enter a Day? ";
+    cin >> Day;
+
+    return Day;
+}
+
+stDate ReadFullDate()
+{
+    stDate Date;
+
+    Date.Day = ReadDay();
+    Date.Month = ReadMonth();
+    Date.Year = ReadYear();
+
+    return Date;
+}
+
+int main()
+{
+    while(true)
+    {
+        system("cls");
+
+        cout << "\nEnter Date1:";
+        stDate Date1 = ReadFullDate();
+
+        cout << "\nEnter Date2:";
+        stDate Date2 = ReadFullDate();
+
+        cout << "\nCompare Result = " << CompareDates(Date1, Date2) << endl;
+
+        
+        system("pause>0");
+    }
+    
+    
+    return 0;
+}
