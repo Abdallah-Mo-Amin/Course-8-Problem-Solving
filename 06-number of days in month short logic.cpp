6b20bd5921ca8dc489965f13a06cfc88d5645c2a
Abdallah-Mo-Amin <abdallahamin567@gmail.com>
2026-08-30 18:17:59 +0300

    Create 06-number of days in month short logic.cpp

---
+++ 06-number of days in month short logic.cpp
@@ -0,0 +1,86 @@
+#pragma warning(disable : 4996)
+
+#include <iostream>
+#include <cstdlib>  
+#include <iomanip> // this library stored the std::setw
+#include <cmath>
+#include <string> // this library stired the string object
+#include <vector>
+#include <cctype>
+#include<sstream>
+#include <algorithm>
+#include <ctime>
+#include <fstream> 
+
+using namespace std;
+
+short ReadYear()
+{
+    short Number = 0;
+
+    cout << "Please enter a year to check? ";
+    cin >> Number;
+
+    while (cin.fail() || cin.peek() != '\n')
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        cout << "Invalid Number , Enter a valid one : ";
+        cin >> Number;
+
+    }
+
+    return Number;
+
+}
+
+short ReadMonth()
+{
+    short Month = 0;
+
+    cout << "\nPlease enter a Month to check? ";
+    cin >> Month;
+
+    while (cin.fail() || cin.peek() != '\n')
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        cout << "\nPlease enter a Month to check? ";
+        cin >> Month;
+    }
+
+    return Month;
+}
+
+bool IsLeapYear(short Year)
+{  
+    return ((Year % 4 == 0 && Year % 100 != 0) || (Year % 400 == 0));
+}
+
+short NumberOfDaysInMonth(short Month, short Year)
+{
+    if (Month < 1 || Month > 12)
+        return 0;
+
+    short NumberOfDays[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+    return (Month == 2) ? (IsLeapYear(Year) ? 29 : 28) : NumberOfDays[Month - 1];
+}
+
+int main()
+{
+ 
+    short Year = ReadYear();
+    short Month = ReadMonth();
+    cout << "\nNumber of Days in Month [" << Month << "] is " << NumberOfDaysInMonth(Month, Year);
+    cout << endl;
+   
+    system("pause>0");
+
+
+    return 0;
+}
+
+
+
