fc93d64720e3f3860e3c6c40e2ba52d641c84cea
Abdallah-Mo-Amin <abdallahamin567@gmail.com>
2026-08-30 18:26:39 +0300

    Create 14-is date1 equals date2.cpp

---
+++ 14-is date1 equals date2.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <cstdlib>  
+#include <iomanip> // this library stored the std::setw
+#include <cmath>
+#include <string> // this library stired the string object
+#include <vector>
+#include <cctype>
+#include<sstream>
+
+using namespace std;
+
+struct stDate
+{
+    short Day = 0;
+    short Month = 0;
+    short Year = 0;
+};
+
+bool IsDate1EqualToDate2(stDate Date1, stDate Date2)
+{
+    return (Date1.Year == Date2.Year) ? ((Date1.Month == Date2.Month) ? ((Date1.Day == Date2.Day) ? true : false) : false) : false;
+}
+
+short ReadYear()
+{
+    short Number = 0;
+
+    cout << "Please enter a year? ";
+    cin >> Number;
+
+    while (cin.fail() || cin.peek() != '\n')
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        cout << "Invalid Number , Enter a valid one : ";
+        cin >> Number;
+
+    }
+
+    return Number;
+
+}
+
+short ReadMonth()
+{
+    short Month = 0;
+
+    cout << "Please enter a Month? ";
+    cin >> Month;
+
+    while (cin.fail() || cin.peek() != '\n')
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        cout << "Please enter a Month? ";
+        cin >> Month;
+    }
+
+    return Month;
+}
+
+short ReadDay()
+{
+    short Day = 0;
+    cout << "\nPlease enter a Day? ";
+    cin >> Day;
+
+    return Day;
+}
+
+stDate ReadFullDate()
+{
+    stDate Date;
+
+    Date.Day = ReadDay();
+    Date.Month = ReadMonth();
+    Date.Year = ReadYear();
+
+    return Date;
+}
+
+int main()
+{
+    while(true)
+    {
+        system("cls");
+        stDate Date1 = ReadFullDate();
+        stDate Date2 = ReadFullDate();
+
+        if (IsDate1EqualToDate2(Date1, Date2))
+            cout << "\nYes, Date1 is equals Date2.\n";
+        else
+            cout << "\nNO, Date1 is NOT equals Date2.\n";
+
+        system("pause>0");
+    }
+    
+    
+    return 0;
+}
